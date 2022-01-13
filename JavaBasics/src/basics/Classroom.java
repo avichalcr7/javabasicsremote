c814fe2b4b6cfdb60921a2c3ba473563804e9e9c
avichalcr7 <64785020+avichalcr7@users.noreply.github.com>
2022-01-13 15:00:42 +0530

    Constructors

--- JavaBasics/src/basics/Classroom.java
+++ JavaBasics/src/basics/Classroom.java
@@ -3,7 +3,10 @@ package basics;
 public class Classroom {
 	public static void main(String[] args) {
 		Student Avichal= new Student();
+		Student bStudent =new Student(1,"Pathak");
 		Employee someEmployee =new Employee();
+		System.out.println(Avichal.id);
+		bStudent.display();
 	}
 
 }
--- JavaBasics/src/basics/Student.java
+++ JavaBasics/src/basics/Student.java
@@ -5,5 +5,18 @@ public class Student {
 	String name;
 	boolean isMale;
 	float stipend;
+	
+	Student(){
+		System.out.println("Akshat I'm about to Construct the box");
+		id=0;
+		name="XYZ";
+	}
+	Student(int i,String nam){
+		id=i;
+		name=nam;
+	}
+	void display() {
+		System.out.println("name is "+name+" stipend is"+stipend);
+	}
 
 }
